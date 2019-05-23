6489ae1ab244c27f5081f99d5905b1b133cdf2e3
sergioro <25378688+sergioro9@users.noreply.github.com>
2019-05-23 02:21:51 -0600

    populate.sh update

--- populate.sh
+++ populate.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-rake db:migrate && rake ffcrm:setup:admin USERNAME=admin PASSWORD=password EMAIL=admin@example.com
-rake db:migrate && rake ffcrm:setup:admin USERNAME=sergioro PASSWORD=password EMAIL=sergioro@example.com
+rake db:migrate &&
+rake ffcrm:demo:load &&
+rake ffcrm:setup:admin USERNAME=admin PASSWORD=password EMAIL=admin@example.com
